d322d2df687136f2beb7d42a20085cadcd80631c
Aniket Mishrikotkar <aniketmishrikotkar@gmail.com>
2023-11-02 12:29:14 +0530

    Update foo

--- dataiku_mlops/foo.py
+++ dataiku_mlops/foo.py
@@ -1,22 +1,18 @@
+import dataikuapi
 from loguru import logger
 
-class Foo:
-    
-    def __init__(self, name: str):
-        self.name = name
+def foo() -> str:
+    """Summary line.
 
-    def foo(self) -> str:
-        """Summary line.
+    Extended description of function.
 
-        Extended description of function.
+    Args:
+        foo (str): Description of arg1
 
-        Args:
-            foo (str): Description of arg1
+    Returns:
+        str: Description of return value
+    """
 
-        Returns:
-            str: Description of return value
-        """
+    logger.info(f"foo function called")
 
-        logger.info(f"foo welcome you {self.name}")
-
-        return "foo"
+    return "foo"
